092ad7861f40a584270434e9fd7ef41e4a132726
Grant Hilgert <Grant@mwc-109215.dhcp.missouri.edu>
2017-09-13 15:45:44 -0500

    Part 2, commit

---
+++ NewFile.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <limits.h>
+
+
+void clear(void){ //Clear the input buffer
+	char testChar = getchar();
+	while(testChar != '\n') {
+		testChar = getchar();
+	}
+
+}
+
+int main(void){
+std::cout << "\nNEW FILE YEAH";
+
+
+}
