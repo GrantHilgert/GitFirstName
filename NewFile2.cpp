9584b6c1367366082186c00dccc2f0d724f02ea5
Grant Hilgert <Grant@mwc-109215.dhcp.missouri.edu>
2017-09-13 15:47:44 -0500

    Part 2, second new file step 9

---
+++ NewFile2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <limits.h>
+
+
+void clear(void){ //Clear the input buffer
+	char testChar = getchar();
+	while(testChar != '\n') {
+		testChar = getchar();
+	}
+
+}
+
+int main(void){
+std::cout << "\nNEW FILE YEAH but this one is number 2222222";
+
+
+}
